66f412abd0ce913eea74208e67503f03bc72e5f7
John <john@iamjohnhenry.com>
2024-08-25 10:26:33 -0700

    Add primer demo

--- demo.mjs
+++
@@ -1,7 +0,0 @@
-// Import the server implementation
-import "./index.mjs";
-$start({ port: 8080 });
-
-addEventListener("fetch", async (event) => {
-  event.respondWith(new Response(event.request.body, { status: 200 }));
-});
